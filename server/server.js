ea30754df1999d0831f0ed1641487b7a08c903c3
Coy1944 <Samuelchincks86@gmail.com>
2022-03-17 15:25:03 -0600

    Update 2

--- server/server.js
+++ server/server.js
@@ -7,18 +7,16 @@ app.use(cors());
 app.use(express.json());
 
 
-// Endpoints
+//Middleware
+app.use(express.static(path.join(__dirname, "../public")));
+
 // app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public"));
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
 // });
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-});
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// });
 
 
 const port = process.env.PORT || 4105;
